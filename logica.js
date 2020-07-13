ff33c0785795cc93800eeb9258b5e585d174fba8
alanromet <alanromet@gmail.com>
2020-07-13 18:26:50 -0400

    Actualizando

--- logica.js
+++ logica.js
@@ -21,9 +21,14 @@ formulario.addEventListener('submit', (event) => {
     buttonSubmit.disabled = true
     setTimeout(() => {
         let nombre = document.querySelector('#nombre').value
-        let apellidos = document.querySelector('#apellidos').value
+        let edad = document.querySelector('#edad').value
         let email = document.querySelector('#email').value
-        let mensaje = 'send?phone=' + telefono + '&text=*¿Cual es tu nombre?*%0A' + nombre + '%0A*¿Cuáles son tus apellidos?*%0A' + apellidos + '%0A*¿Cuál es tu mensaje*%0A' + email + ''
+        let sms = document.querySelector('#sms').value
+        let mensaje = 'send?phone=' + telefono + 
+        '&text=*Hola soy*%0A' + nombre + '' // Nombre y apellido
+        + '%0A*Tengo*%0A' + edad + ' años' //Edad
+        + '%0A*Tu mensaje*%0A' + sms    // Mesaje
+        + '%0A*¿Mi correo es*%0A' + email + '' //Email
         if(isMobile()) {
             window.open(urlMobile + mensaje, '_blank')
         }else{
